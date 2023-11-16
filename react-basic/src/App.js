b35d0ca30e83103e57c3671579f89dd71e3dfa5e
yangxiao123 <1106988961@qq.com>
2023-11-16 16:09:10 +0800

    reactStatus

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -17,7 +17,35 @@ const list = [
   {id:1003, name:"Angular"}
 ]
 
+const isLogin = true
+
+// 定义文章类型
+const type = 1
+
+// 定义函数（根据type返回不同模板）
+function artical(type){
+  if(type === 0){
+    return <div>我是无图文章</div>
+  }else if(type === 1){
+    return <div>我是单图模式</div>
+  }else{
+    return <div>我是三图模式</div>
+  }
+}
+
+function handleClick(e,name){
+  console.log("被点击了");
+  console.log(e);
+  console.log(name);
+  console.log(this);
+}
+
+function Button(){
+  return <button>click me</button>
+}
+
 function App() {
+  
   return (
     <div className="App">
       this is a app
@@ -37,6 +65,18 @@ function App() {
           {list.map(item => <li key={item.id}>{item.name}</li>)}
         </ul>
       </div>
+
+      {/* 逻辑与 && */}
+      <div>
+        {isLogin && <span>this is a span</span>}
+        {isLogin ? <span>tom</span> : <span>jerry</span>}
+      </div>
+
+      {artical(type)}
+
+      <button onClick={(e) => handleClick(e,"tom")}>点击</button>
+
+      <Button></Button>
     </div>
   );
 }
